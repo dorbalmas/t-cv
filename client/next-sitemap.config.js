271fbf504d8128ee89d01a5cef699bc5bd4db106
dorbalmas <dorbal31@gmail.com>
2023-01-11 16:45:37 +0200

    sitemap

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://rxresu.me',
+  siteUrl: 'https://tivlotcv.com',
   changefreq: 'monthly',
   generateRobotsTxt: true,
 };
